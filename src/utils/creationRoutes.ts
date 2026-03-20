e90ad892d2c4d18e6d2004f065d77cc1d4f315b1
bryan-menoux <bryan.menoux@edu.univ-fcomte.fr>
2026-03-20 15:01:09 +0100

    modif des redirects

--- src/utils/creationRoutes.ts
+++ src/utils/creationRoutes.ts
@@ -24,7 +24,7 @@ export function getCreationViewUrl(
     return `/creation/projets/`;
   }
 
-  return `/creation/produits/${slugPath}`;
+  return getCreationEditUrl("produit", id, slug);
 }
 
 export function getCreationEditUrl(
